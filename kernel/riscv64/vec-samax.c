7a355da673e56941a1f81f898fa42fdf2ad8af0a
Jerry Zhao <jerryz123@berkeley.edu>
2018-04-16 02:38:17 -0700

    Added all vector sblas1 kernels

---
+++ kernel/riscv64/vec-samax.c
@@ -0,0 +1,100 @@
+/***************************************************************************
+Copyright (c) 2013, The OpenBLAS Project
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
+the documentation and/or other materials provided with the
+distribution.
+3. Neither the name of the OpenBLAS project nor the names of
+its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE OPENBLAS PROJECT OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+/**************************************************************************************
+* 2013/09/14 Saar
+*	 BLASTEST float		: OK
+* 	 BLASTEST double	: OK
+* 	 CTEST			: NoTest
+* 	 TEST			: NoTest
+*
+**************************************************************************************/
+
+#include "common.h"
+#include <math.h>
+#include "rvv.h"
+
+#if defined(DOUBLE)
+
+#define ABS fabs
+
+#else
+
+#define ABS fabsf
+
+#endif
+
+
+FLOAT CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x)
+{
+	BLASLONG i=0;
+	BLASLONG ix=0;
+	FLOAT maxf=0.0;
+
+	if (n <= 0 || inc_x <= 0) return(maxf);
+        resetvcfg();
+        setvcfg0(VFP32,    // *x
+                 SFP32,    //
+                 VFP32,    // *acc
+                 VFP32);   // *accshift
+        int vl = 0;
+        int ct = 0;
+        setvl(vl, n);
+        while (vl > 1)
+          {
+            ct++;
+            vl = vl >> 1;
+          }
+        vl = 1 << ct;
+        setvl(vl, vl);
+        asm volatile ("vsne    v2, v2, v2");   // v2 =0
+	while(i < n)
+	{
+          while (n - i < vl)
+            {
+              asm volatile ("vslide v3, v2, %0" : : "r" (vl >> 1));
+              setvl(vl, vl >> 1);
+              asm volatile ("vmax   v2, v2, v3");
+            }
+          asm volatile ("vlds  v0, 0(%0), %1" : : "r" (&x[ix]), "r" (inc_x << 2));
+          asm volatile ("vsgnjx v0, v0, v0");
+          asm volatile ("vmax v2, v0, v2"); // acc[] = x[]*y[]
+          i = i + vl;
+          ix = ix + vl * inc_x;
+	}
+        while (vl > 1)
+          {
+            asm volatile ("vslide v3, v2, %0" : : "r" (vl >> 1));
+            setvl(vl, vl >> 1);
+            asm volatile ("vmax   v2, v2, v3");
+          }
+        asm volatile ("vst      v2, 0(%0)" : : "r" (&maxf));
+	return(maxf);
+
+}
+
+
